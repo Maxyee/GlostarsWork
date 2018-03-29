29ef498e7184385cd1c8068204ac310744bd6400
Maxyee <eyaminkhan00@gmail.com>
2018-03-29 18:31:35 +0600

    how to login with facebook using asp.net mvc

---
+++ LoginWithFacebook/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Facebook;
+using Newtonsoft.Json;
+
+namespace LoginWithFacebook.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult FbLogin()
+        {
+            ViewBag.Message = "Facebook Login Page";
+            return View();
+        }
+
+        private Uri RedirectUri
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(Request.Url);
+                uriBuilder.Query = null;
+                uriBuilder.Fragment = null;
+                uriBuilder.Path = Url.Action("FacebookCallback");
+                return uriBuilder.Uri;
+            }
+        }
+
+        [AllowAnonymous]
+        public ActionResult Facebook()
+        {
+            var fb = new FacebookClient();
+            var loginUrl = fb.GetLoginUrl(new
+            {
+                client_id = "Your facebook api Client id here",
+                client_secret = "Your facebook api secrete key here",
+                rediret_uri = RedirectUri.AbsoluteUri,
+                response_type = "code",
+                scope="email"
+            });
+            return Redirect(loginUrl.AbsoluteUri);
+        }
+
+        public ActionResult FacebookCallback(string code)
+        {
+            var fb = new FacebookClient();
+            dynamic result = fb.Post("oauth/access_token", new
+            {
+                client_id = "Your facebook api Client id here",
+                client_secret = "Your facebook api secrete key here",
+                rediret_uri = RedirectUri.AbsoluteUri,
+                code = code
+            });
+            var accessToken = result.access_token;
+            Session["AccessToken"] = accessToken;
+            fb.AccessToken = accessToken;
+            dynamic me =
+                fb.Get(
+                    "me?fields=link,first_name,currency,last_name,email,gender,locale,timezone,verified,picture,age_range");
+            string email = me.email;
+            string lastname = me.last_name;
+            string picture = me.picture.data.url;
+            FormsAuthentication.SetAuthCookie(email,false);
+            return RedirectToAction("FbLogin", "Home");
+        }
+    }
+}
