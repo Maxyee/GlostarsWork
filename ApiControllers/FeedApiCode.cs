f263c42d23ddbf3d643dff4b83f06a30bf5a33e2
Maxyee <eyaminkhan00@gmail.com>
2018-05-16 03:09:18 +0600

    Added the blocking mechanisum to the feed api

--- ApiControllers/FeedApiCode.cs
+++ ApiControllers/FeedApiCode.cs
@@ -9,3 +9,14 @@ var mutualFollowerActivites = Db.Activities.Where(
     .ToList();
     
 //feed main logic ends here -------------->
+
+/*-------blocking system section julhas starts-------*/
+                var mutualFollowerActivites = Db.Activities.Where(
+                   a => ((a.User.FollowingList.Any(b => MyId == b.Id) && a.User.FollowerList.Any(b => MyId == b.Id) && a.User.BlockUsersList.Any(b => b.BlockedbyId == MyId && b.Unblock == false))
+                       || (a.User.FollowerList.Any(b => MyId == b.Id) && a.Picture.Privacy != "friends" && a.User.BlockUsersList.Any(b => b.BlockedbyId == MyId && b.Unblock == false))
+                       || (a.UserId == MyId)) && !listPhoto.Contains(a.PictureId) && !a.Picture.IsCompeting)
+                   .OrderByDescending(q => q.Date)
+                   //.Skip(count*10)
+                   .Take(10)
+                   .ToList();
+/*-------blocking system section julhas starts-------*/
